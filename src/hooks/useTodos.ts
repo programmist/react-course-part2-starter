0c635c4e3f98ab9929d133b38ed0d52437828e8e
Tony Childs <tony.childs@gmail.com>
2023-09-07 14:43:30 -0500

    Factor out useTodo hook

---
+++ src/hooks/useTodos.ts
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export interface Todo {
+  id: number;
+  title: string;
+  userId: number;
+  completed: boolean;
+}
+
+const useTodos = () => {
+  const endpoint = "https://jsonplaceholder.typicode.com/todos";
+  return useQuery<Todo[], Error>({
+    queryKey: ["todos"],
+    queryFn: () => axios.get<Todo[]>(endpoint).then((res) => res.data),
+  });
+};
+
+export default useTodos;
--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,31 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-
-/**
- * TODOs
- * - Retry failed requests
- * - Automatically refresh data
- * - Add data caching
- */
-
-interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
+import useTodos, { Todo } from "../hooks/useTodos";
 
 const TodoList = () => {
-  const fetchTodos = () =>
-    axios
-      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.data);
+  const { data: todos, error, isLoading } = useTodos();
 
-  const { data: todos, error } = useQuery<Todo[], Error>({
-    queryKey: ["todos"],
-    queryFn: fetchTodos,
-  });
+  if (isLoading) return <p>Loading...</p>;
 
   if (error) return <p>{error.message}</p>;
 
