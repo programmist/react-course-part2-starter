ee524f9b8966f353d8d3d909cdbf3e057fd42d61
Tony Childs <tony.childs@gmail.com>
2023-09-08 18:29:06 -0500

    Use mutate to add Todo and update list

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,10 +1,50 @@
-import { useRef } from 'react';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRef } from "react";
+import { Todo } from "../hooks/useTodos";
+import axios from "axios";
+interface Props {
+  onAddTodo: (todo: string | undefined) => void;
+}
 
-const TodoForm = () => {
+const TodoForm = ({ onAddTodo }: Props) => {
+  const queryClient = useQueryClient();
+  const addTodo = useMutation({
+    mutationFn: (todo: Todo) =>
+      axios
+        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
+        .then((res) => res.data),
+    onSuccess: (savedTodo, newTodo) => {
+      // APPROACH 1: Invalidate cache so newly added data is fetched
+      // and displayed. Note: This won't work on JSONPlaceholder fake API
+      // queryClient.invalidateQueries({
+      //   queryKey: ["todos"],
+      // });
+
+      // APPROACH 2: Updating the data in the cache directly
+      // Add the new Todo directly to the top of the cached list of Todos
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+        savedTodo,
+        ...(todos || []),
+      ]);
+    },
+  });
   const ref = useRef<HTMLInputElement>(null);
 
   return (
-    <form className="row mb-3">
+    <form
+      className="row mb-3"
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref?.current?.value) {
+          addTodo.mutate({
+            id: 0,
+            title: ref?.current?.value,
+            completed: false,
+            userId: 1, // hardcoded for testing
+          });
+        }
+      }}
+    >
       <div className="col">
         <input ref={ref} type="text" className="form-control" />
       </div>
