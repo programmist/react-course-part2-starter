a64c7baf9438378c0e00a96b7e4fd3542532ae21
Tony Childs <tony.childs@gmail.com>
2023-09-14 16:37:08 -0500

    clean up counter code

--- src/state-management/counter/Counter.tsx
+++ src/state-management/counter/Counter.tsx
@@ -1,5 +1,17 @@
 import { useReducer, useState } from "react";
-import counterReducer from "./reducers/counterReducer";
+
+interface Action {
+  type: "INCREMENT" | "RESET";
+}
+
+const counterReducer = (state: number, action: Action): number => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "RESET":
+      return 0;
+  }
+};
 
 const Counter = () => {
   const [value, dispatch] = useReducer(counterReducer, 0);
