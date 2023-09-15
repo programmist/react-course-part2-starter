2e9478074bcbe048c373e6e0f30f3d9f14f43768
Tony Childs <tony.childs@gmail.com>
2023-09-14 19:55:40 -0500

    implement zustand store for counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { AuthProvider } from "./state-management/auth";
+import { Counter } from "./state-management/counter";
 import HomePage from "./state-management/HomePage";
 import NavBar from "./state-management/NavBar";
 import { TasksProvider } from "./state-management/tasks";
@@ -8,6 +9,7 @@ function App() {
   return (
     <AuthProvider>
       <TasksProvider>
+        <Counter />
         <NavBar />
         <HomePage />
       </TasksProvider>
--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,12 +1,17 @@
 import { LoginStatus } from "./auth";
+import useCounterStore from "./counter/store";
 import useTasks from "./tasks/useTasks";
 
 const NavBar = () => {
   const { tasks } = useTasks();
+  const { counter } = useCounterStore();
 
   return (
     <nav className="navbar d-flex justify-content-between">
-      <span className="badge text-bg-secondary">{tasks.length}</span>
+      <span className="badge text-bg-secondary">Counter: {counter}</span>
+      <span className="badge text-bg-secondary">
+        Task Count: {tasks.length}
+      </span>
       <LoginStatus />
     </nav>
   );
