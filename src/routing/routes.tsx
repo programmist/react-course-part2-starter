cdec6c8ab9d2a36492e30594cc8f9071c876a50c
Tony Childs <tony.childs@gmail.com>
2023-09-16 11:25:18 -0500

    add navbar and ntested routes

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,14 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
-import UserListPage from "./UserListPage";
-import ContactPage from "./ContactPage";
+import Layout from "./Layout";
 import UserDetailPage from "./UserDetailPage";
+import UserListPage from "./UserListPage";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/users", element: <UserListPage /> },
-  { path: "/users/:id", element: <UserDetailPage /> },
-  { path: "/contact", element: <ContactPage /> },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "users", element: <UserListPage /> },
+      { path: "users/:id", element: <UserDetailPage /> },
+    ],
+  },
 ]);
 
 export default router;
