f1c0b9d3942d0cc232d70fe8fde48bd3d504c412
Tony Childs <tony.childs@gmail.com>
2023-09-16 11:10:27 -0500

    route data hooks

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,39 +1,55 @@
 import { useEffect, useState } from "react";
-import { useParams, useSearchParams } from "react-router-dom";
+import {
+  useLocation,
+  useNavigate,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
 import useUsers, { User } from "./hooks/useUsers";
 
 const UserDetailPage = () => {
   // URL params
   const { id } = useParams();
+  const nav = useNavigate();
 
   // Query string params
   const [searchParams, setSearchParams] = useSearchParams();
-
+  const location = useLocation();
   const [user, setUser] = useState<User | undefined>();
   const { get } = useUsers();
 
-  console.log(searchParams.toString());
-  console.log(searchParams.get("state"));
-
   useEffect(() => {
     get(id as string).then((user) => setUser(user));
+    console.log("useLocation", location);
+    console.log(searchParams.toString());
+    console.log(searchParams.get("state"));
   }, []);
 
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Name</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>{user?.id}</td>
-          <td>{user?.name}</td>
-        </tr>
-      </tbody>
-    </table>
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{user?.id}</td>
+            <td>{user?.name}</td>
+          </tr>
+        </tbody>
+      </table>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          nav("/");
+        }}
+      >
+        <button className="btn btn-primary">Home</button>
+      </form>
+    </>
   );
 };
 
