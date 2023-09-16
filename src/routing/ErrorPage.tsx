d3dc4259c873b02881dd5931ae400095db6bdc24
Tony Childs <tony.childs@gmail.com>
2023-09-16 12:56:06 -0500

    Handle invalid page/route and other errors

--- src/routing/ErrorPage.tsx
+++ src/routing/ErrorPage.tsx
@@ -1,8 +1,20 @@
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
 const ErrorPage = () => {
+  const error = useRouteError();
+  const isRoutingError = isRouteErrorResponse(error);
+
+  // TODO: use logging service like Sentry
+  console.log(error);
+
   return (
     <>
       <h1>Oops...</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        {isRoutingError
+          ? "Invalid Page"
+          : "Sorry, an unexpected error has occurred."}
+      </p>
     </>
   );
 };
