f150132b6bf830ae0a2df26e6e98ecc6342d48f2
Tony Childs <tony.childs@gmail.com>
2023-09-16 09:58:46 -0500

    Add useSearchParams

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import useUsers, { User } from "./hooks/useUsers";
 
 const UserDetailPage = () => {
+  // URL params
   const { id } = useParams();
+
+  // Query string params
+  const [searchParams, setSearchParams] = useSearchParams();
+
   const [user, setUser] = useState<User | undefined>();
   const { get } = useUsers();
+
+  console.log(searchParams.toString());
+  console.log(searchParams.get("state"));
 
   useEffect(() => {
     get(id as string).then((user) => setUser(user));
@@ -14,8 +22,10 @@ const UserDetailPage = () => {
   return (
     <table className="table">
       <thead>
-        <th scope="col">ID</th>
-        <th scope="col">Name</th>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Name</th>
+        </tr>
       </thead>
       <tbody>
         <tr>
