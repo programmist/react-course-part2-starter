ae5fd7083cb5e92bd607caf195742c2160eed7bf
Tony Childs <tony.childs@gmail.com>
2023-09-16 13:12:31 -0500

    Create private route (only logged-in users)

--- src/routing/LoginPage.tsx
+++ src/routing/LoginPage.tsx
@@ -1,5 +1,43 @@
-const LoginPage = () => {
-  return <div>Login</div>;
-};
+const LoginPage = () => (
+  <div style={{ width: "50%" }}>
+    <form>
+      <div className="form-group">
+        <label htmlFor="exampleInputEmail1">Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          placeholder="Enter email"
+        />
+        <small id="emailHelp" className="form-text text-muted">
+          We'll never share your email with anyone else.
+        </small>
+      </div>
+      <div className="form-group">
+        <label htmlFor="exampleInputPassword1">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="exampleInputPassword1"
+          placeholder="Password"
+        />
+      </div>
+      <div className="form-group form-check">
+        <input
+          type="checkbox"
+          className="form-check-input"
+          id="exampleCheck1"
+        />
+        <label className="form-check-label" htmlFor="exampleCheck1">
+          Check me out
+        </label>
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  </div>
+);
 
 export default LoginPage;
--- src/routing/hooks/useAuth.ts
+++ src/routing/hooks/useAuth.ts
@@ -1,5 +1,8 @@
-const useAuth = () => ({ user: { id: 1, name: 'Mosh' } });
+/**
+ * Fake Auth hook for testing
+ */
 
-// const useAuth = () => ({ user: null });
+const useAuth = (inValidUser: boolean = false) =>
+  inValidUser ? { user: null } : { user: { id: 1, name: "Mosh" } };
 
 export default useAuth;
